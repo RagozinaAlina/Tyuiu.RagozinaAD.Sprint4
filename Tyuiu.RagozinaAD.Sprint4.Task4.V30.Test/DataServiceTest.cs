e5d63b7fc91adcda56c327bd206cabececf2f03a
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-27 15:59:41 +0500

    Task4

---
+++ Tyuiu.RagozinaAD.Sprint4.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.RagozinaAD.Sprint4.Task4.V30.Lib; 
+namespace Tyuiu.RagozinaAD.Sprint4.Task4.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValudCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5] {{2, 4, 2, 7, 7 },
+                                       { 4, 7, 9, 9, 7 },
+                                       { 8, 5, 5, 6, 5 },
+                                       { 7, 7, 5, 2, 9 },
+                                       { 9, 3, 7, 9, 8,} };
+            int[,] res = ds.Calculate(matrix);
+            int[,] wait = {{2, 4, 2, 0, 0 },
+                           { 4, 0, 0, 0, 0 },
+                           { 8, 0, 0, 6, 0 },
+                           { 0, 0, 0, 2, 0 },
+                           { 0, 0, 0, 0, 8,} };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint4.Task4.V30/Program.cs
@@ -0,0 +1,73 @@
+﻿using Tyuiu.RagozinaAD.Sprint4.Task4.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
+        Console.WriteLine("* заполненный статическими значениями в диапазоне от 2 до 9.              *");
+        Console.WriteLine("* Найдите максимальный элемент в последней строке массива.                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите количество строк массива:");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите количество столбцов массива: ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matrix = new int[rows, columns];
+
+        Console.WriteLine("***************************************************************************");
+
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"Введите элемент массива {i},{j}:");
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("Введённый массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬАТ:                                                               *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] res = ds.Calculate(matrix);
+
+        Console.WriteLine("Массив без нечётных элементов:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{res[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
