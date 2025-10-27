1c37bd225c72faee8a6887f9158ac52d4a0c8107
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-27 13:16:25 +0500

    Task2

---
+++ Tyuiu.RagozinaAD.Sprint4.Task2.V24.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.RagozinaAD.Sprint4.Task2.V24.Lib;
+namespace Tyuiu.RagozinaAD.Sprint4.Task2.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 8, 1, 5, 4, 2, 1, 2, 7, 6, 1 };
+            Assert.AreEqual(15, ds.Calculate(array));
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint4.Task2.V24/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.RagozinaAD.Sprint4.Task2.V24.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 12 элементов                     *");
+        Console.WriteLine("* в диапазоне от 4 до 9 подсчитать сумму нечетных элементов массива.      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите количество элементов массива: ");
+        int len = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[len];
+
+        for (int i = 0; i <= len - 1; i++)
+        {
+            array[i] = rnd.Next (4,9);
+        }
+        Console.WriteLine("Массив:");
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine(array[i]+"\t");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(array);
+
+        Console.WriteLine("Сумма нечетных элементов = "+res);
+        Console.ReadKey();
+
+    }
+}
