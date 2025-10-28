35c1005312636fa18871120cd5df1ad90b1d27a2
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-28 21:44:18 +0500

    Task7

---
+++ Tyuiu.RagozinaAD.Sprint4.Task7.V22.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.RagozinaAD.Sprint4.Task7.V22.Lib;
+namespace Tyuiu.RagozinaAD.Sprint4.Task7.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            int rows = 3;
+            int columns = 5;
+            int[,] mtrx = new int[rows, columns];
+            string str = "695324951753684";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 9216;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint4.Task7.V22/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.RagozinaAD.Sprint4.Task7.V22.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int rows = 3;
+        int columns = 5;
+        int[,] mtrx = new int[rows, columns];
+        string str = "695324951753684";
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Дана строка из одноразрядных цифр. Преобразуйте ее в матрицу 5 на 3     *");
+        Console.WriteLine("* и подсчитайте произведение четных чисел.                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int index = 0;
+
+        Console.WriteLine("\nМассив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{str[index]} \t");
+                index++;
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(rows ,columns ,str);
+
+        Console.WriteLine("Произведение четных чисел = " + res);
+        Console.ReadKey();
+
+    }
+}
